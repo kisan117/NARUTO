473298ea53f4c7af1d4ec9714efbf4a5527b5b84
Jwnwnj <149147034+Jwnwnj@users.noreply.github.com>
2024-11-04 10:08:48 +0530

    Update devil.js

--- Priyansh/commands/devil.js
+++ Priyansh/commands/devil.js
@@ -18,7 +18,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("devil") || 
 react.includes("aryan")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗣𝗿𝗼𝗳𝗶𝗹𝗲 𝗛𝗲𝗿𝗲★\n\n✦𝗠𝗥.𝗗𝗘𝗩𝗜𝗟 𝗞𝗜𝗡𝗚✦\n\n https://www.facebook.com/ \n\n☞★★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 :  𝐌𝐫..𝐃𝐞𝐯𝐢𝐥🩷🪽✦`🥀🦋",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗣𝗿𝗼𝗳𝗶𝗹𝗲 𝗛𝗲𝗿𝗲★\n\n✦𝗠𝗥.𝗗𝗘𝗩𝗜𝗟 𝗞𝗜𝗡𝗚✦\n\n https://www.facebook.com/61560621821421 \n\n☞★★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 :  𝐌𝐫..𝐃𝐞𝐯𝐢𝐥🩷🪽✦`🥀🦋",
 				attachment: fs.createReadStream(__dirname + `https://i.imgur.com/5xEAkN2.jpeg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
